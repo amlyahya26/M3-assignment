a82098b55763d8fc10f0950d7e02e0dd1474167a
Amal Yahya <amlyahya742@gmail.com>
2024-02-28 14:46:56 -0800

    https://github.com/amlyahya26/M3-assignment.git

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,17 +1,17 @@
 
 // Prompt the user for two integers
-        const firstNumber = parseInt(prompt("Enter the first integer:"));
-        const secondNumber = parseInt(prompt("Enter the second integer:"));
+const firstNumber = parseInt(prompt("Enter the first integer:"));
+const secondNumber = parseInt(prompt("Enter the second integer:"));
 
 // Compare the two numbers
-        let result;
-        if (firstNumber > secondNumber) {
-            result = `The larger number is: ${firstNumber}`;
-        } else if (secondNumber > firstNumber) {
-            result = `The larger number is: ${secondNumber}`;
-        } else {
-            result = "Both numbers are equal.";
-        }
+let result;
+if (firstNumber > secondNumber) {
+result = `The larger number is: ${firstNumber}`;
+} else if (secondNumber > firstNumber) {
+result = `The larger number is: ${secondNumber}`;
+} else {
+result = "Both numbers are equal.";
+}
 
- // Display the result in the browser window
-        document.write(result);
+// Display the result in the browser window
+document.write(result);

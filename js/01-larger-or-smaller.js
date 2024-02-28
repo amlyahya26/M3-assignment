b26875ebbd59c688b35e2b75ca8e49674001c1e4
Amal Yahya <amlyahya742@gmail.com>
2024-02-28 14:44:01 -0800

    M3-assignment

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,17 @@
+
+// Prompt the user for two integers
+        const firstNumber = parseInt(prompt("Enter the first integer:"));
+        const secondNumber = parseInt(prompt("Enter the second integer:"));
+
+// Compare the two numbers
+        let result;
+        if (firstNumber > secondNumber) {
+            result = `The larger number is: ${firstNumber}`;
+        } else if (secondNumber > firstNumber) {
+            result = `The larger number is: ${secondNumber}`;
+        } else {
+            result = "Both numbers are equal.";
+        }
+
+ // Display the result in the browser window
+        document.write(result);
